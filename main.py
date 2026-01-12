ebb5c06039faf53a8331c6aa3218f3251a4834a9
dinhanhx <dinhanhx@gmail.com>
2026-01-12 11:26:26 +0700

    Add /docx2pdf endpoint

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from pathlib import Path
+from typing import Literal
 from uuid import uuid4
 
 import aiofiles
@@ -14,8 +15,6 @@ load_dotenv()
 TMP_DIR = Path(os.getenv("TMP_DIR", "tmp/"))
 TMP_DIR.mkdir(exist_ok=True, parents=True)
 MAX_FILE_SIZE = int(os.getenv("MAX_FILE_SIZE", str(10 * 1024 * 1024)))  # 10 MB
-
-DOC_EXT = ".doc"
 
 app = FastAPI()
 
@@ -44,39 +43,55 @@ def cleanup(*files: Path):
             print(f"Failed to delete {file}: {str(e)}")
 
 
-@app.post("/convert")
-async def convert(file: UploadFile):
-    # Limit file size
+def validate_file(
+    file: UploadFile,
+    file_ext_to_check: Literal[".doc", ".docx"],
+) -> JSONResponse | None:
     if file.size is None or file.size > MAX_FILE_SIZE:
         return JSONResponse(
             status_code=400,
             content={"error": f"File size exceeds the maximum limit of {MAX_FILE_SIZE} bytes."},
         )
-
-    original_file_stem = Path(str(file.filename)).stem
-    original_file_extension = Path(str(file.filename)).suffix
-    # Make sure it is a .doc file
-    if original_file_extension.lower() != ".doc":
+    if Path(str(file.filename)).suffix.lower() != file_ext_to_check:
         return JSONResponse(
             status_code=400,
-            content={"error": "Invalid file type. Only .doc files are allowed."},
+            content={"error": f"Invalid file type. Only {file_ext_to_check} files are allowed."},
         )
+    return None
 
-    file_uuid = str(uuid4())
-    file_input = TMP_DIR.joinpath(f"{file_uuid}{original_file_extension}")
 
-    # Save the uploaded file
+async def save_uploaded_file(file: UploadFile, file_input: Path) -> JSONResponse | None:
     try:
         await file.seek(0)
         async with aiofiles.open(file_input, "wb") as file_input_pointer:
             while content := await file.read(1024):
                 await file_input_pointer.write(content)
         await file.close()
+        return None
     except Exception as e:
         return JSONResponse(
             status_code=500,
             content={"error": f"Failed to save the uploaded file: {str(e)}"},
         )
+
+
+@app.post("/convert")
+async def convert(file: UploadFile):
+    # Validate the file
+    any_response = validate_file(file, ".doc")
+    if any_response is not None:
+        return any_response
+
+    original_file_stem = Path(str(file.filename)).stem
+    original_file_extension = Path(str(file.filename)).suffix
+
+    file_uuid = str(uuid4())
+    file_input = TMP_DIR.joinpath(f"{file_uuid}{original_file_extension}")
+
+    # Save the uploaded file
+    any_response = await save_uploaded_file(file, file_input)
+    if any_response is not None:
+        return any_response
 
     file_output = TMP_DIR.joinpath(f"{file_uuid}.docx")
 
@@ -91,6 +106,46 @@ async def convert(file: UploadFile):
             path=file_output,
             filename=f"{original_file_stem}.docx",
             media_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            background=BackgroundTask(cleanup, file_input, file_output),
+        )
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
+        return JSONResponse(
+            status_code=500,
+            content={"error": f"File conversion failed: {str(e)}"},
+        )
+
+
+@app.post("/docx2pdf")
+async def docx2pdf(file: UploadFile):
+    # Validate the file
+    any_response = validate_file(file, ".docx")
+    if any_response is not None:
+        return any_response
+
+    original_file_stem = Path(str(file.filename)).stem
+    original_file_extension = Path(str(file.filename)).suffix
+
+    file_uuid = str(uuid4())
+    file_input = TMP_DIR.joinpath(f"{file_uuid}{original_file_extension}")
+
+    # Save the uploaded file
+    any_response = await save_uploaded_file(file, file_input)
+    if any_response is not None:
+        return any_response
+
+    file_output = TMP_DIR.joinpath(f"{file_uuid}.docx")
+
+    # Convert the file using docx2pdf cli
+    try:
+        subprocess.run(
+            ["docx2pdf", str(file_input), str(file_output)],
+            check=True,
+            timeout=int(os.getenv("TIMEOUT", "300")),
+        )
+        return FileResponse(
+            path=file_output,
+            filename=f"{original_file_stem}.pdf",
+            media_type="application/pdf",
             background=BackgroundTask(cleanup, file_input, file_output),
         )
     except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as e:
